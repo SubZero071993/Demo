39676eae6834ac0debae90cfeb1aaec8dfeac337
SubZero071993 <al_hos_sam@hotmail.com>
2025-08-06 11:35:11 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ data = [
     ["Cios Select FD VA20", "22-07-25", 20087, "warehouse", "", ""],
     ["Cios Connect", "25-05-25", 21521, "Al-Rawdhah Hospital (until we submit Cios Select)", "Ayman Tamimi", ""],
     ["Cios Fusion", "27-07-25", 31181, "warehouse", "", ""],
-    ["Cios Alpha VA20", "29-05-25", 13020, "Al-Hayyat Hospital (until they receive their C-arm)", "Ammar", ""],
+    ["Cios Alpha VA20", "05-08-25", 13020, "warehouse", "", ""],
     ["Cios Alpha VA30", "03-07-25", 43815, "Aster Sanad Hospital", "Ammar", ""],
     ["Cios Spin VA30", "10-07-25", 50097, "Johns Hopkins Aramco Hospital", "Ayman Tamimi", "Ali"]
 ]
