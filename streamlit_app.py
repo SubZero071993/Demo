76bf7d03b55d97b63c2f46be65cef02f51ba1b48
SubZero071993 <al_hos_sam@hotmail.com>
2025-08-07 16:49:31 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.image("https://i.postimg.cc/vB9fTQSz/85-EEED00-2-AC8-4201-BDB1-9978-B32000-D9-removebg-preview.png", width=500)
+st.image("https://i.postimg.cc/qBwX37vb/Adobe-Express-file.png", width=500)
 
 st.set_page_config(layout="wide")
 st.title("📎Documents ") 
