25b35812ec9d5f5817356dcd9c1fe6120ba8912b
SubZero071993 <al_hos_sam@hotmail.com>
2025-08-07 14:40:16 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-col1, col2, col3, col4, col5, col6, col7 = st.columns([10, 1, 10, 1, 1, 1, 1])  # تقسيم الصفحة إلى 3 أعمدة بنسبة مختلفة
+col1, col2, col3, col4, col5, col6, col7 = st.columns([10, 1, 10, 1, 1, 1, 1])  # تقسيم الصفحة إلى 7 أعمدة بنسبة مختلفة
 
 # الشعار اليسار (سيمنس)
 with col1:
@@ -45,7 +45,7 @@ with col1:
 
 # الشعار في الوسط (الكاد)
 with col3:
-    st.image("https://iili.io/FiS0iNa.png", width=150)
+    st.image("https://iili.io/FiS0iNa.png", width=300)
 
 
 selected_page = st.session_state.get("selected_page")
