ad217a902ebf998c7bb1b74e01d22b68a5cf5116
SubZero071993 <al_hos_sam@hotmail.com>
2025-08-07 15:34:08 +0300

    Update Schedule.py

--- Schedule.py
+++ Schedule.py
@@ -83,39 +83,3 @@ st.markdown("### 🎨 Final Results:")
 styled_df = edited_df.style.apply(highlight_row, axis=1)
 st.dataframe(styled_df, use_container_width=True)
 
-st.set_page_config(layout="wide")
-st.title("📎Brochures and Configurations ") 
-devices = [
-    {
-        "Device": "Cios Connect",
-        "Brochure": "https://smallpdf.com/file#s=a25c199b-1739-4745-a81a-e1725caba96c",
-        "Configuration": "https://smallpdf.com/file#s=57bb6fa2-cba3-4971-bbce-0049462e9165"
-    },
-    {
-        "Device": "Cios Fusion",
-        "Brochure": "https://smallpdf.com/file#s=cec6d7a8-4b7a-47c5-bfa2-a098da63f422",
-        "Configuration": "https://smallpdf.com/file#s=dfd03daa-a6f0-4ad7-84a9-235b585cbf38"
-    },
-    {
-        "Device": "Cios Alpha VA30",
-        "Brochure": "https://smallpdf.com/file#s=0371cf4c-e55e-48bb-82bb-ffd6aa2cf9d2",
-        "Configuration": "https://smallpdf.com/file#s=b07cc63b-0327-4b5a-b2f6-59fc2ec66e2b"
-    },
-    {
-        "Device": "Cios Spin",
-        "Brochure": "https://smallpdf.com/file#s=3b4c2ced-54cb-48d4-8124-9e9f8beb5f15",
-        "Configuration": "https://smallpdf.com/file#s=9a377c59-e004-4804-8d3c-6c8f2e53309d"
-    }
-]
-
-import streamlit as st
-
-icon_brochure = "📄"
-icon_config = "🛠️"
-
-for device in devices:
-    st.markdown(f"### {device['Device']}")
-    st.markdown(
-        f"{icon_brochure} Brochure: [Click here]({device['Brochure']})  \n"
-        f"{icon_config} Configuration: [Click here]({device['Configuration']})"
-    )
