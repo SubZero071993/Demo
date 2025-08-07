517d1f52ad395a0d707fdbb14e41646ffb265ba8
SubZero071993 <al_hos_sam@hotmail.com>
2025-08-07 16:33:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-str.image:("https://imgur.com/a/LnHRGWY", width==200)
+str.image:("https://imgur.com/a/LnHRGWY", width=50)
 
 st.set_page_config(layout="wide")
 st.title("📎Documents ") 
