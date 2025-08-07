f82f2336393f0a7c3a1d9b2298394513dd4fed39
SubZero071993 <al_hos_sam@hotmail.com>
2025-08-07 15:59:18 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,20 @@ devices = [
     {
         "Device": "Cios Fusion",
         "Brochure": "https://smallpdf.com/file#s=cec6d7a8-4b7a-47c5-bfa2-a098da63f422",
-        "Configuration": "https://smallpdf.com/file#s=dfd03daa-a6f0-4ad7-84a9-235b585cbf38"
+        "Configuration": "https://smallpdf.com/file#s=dfd03daa-a6f0-4ad7-84a9-235b585cbf38",
+        "Data Sheet": "https://smallpdf.com/file#s=57bb6fa2-cba3-4971-bbce-0049462e9165" 
     },
     {
         "Device": "Cios Alpha VA30",
         "Brochure": "https://smallpdf.com/file#s=0371cf4c-e55e-48bb-82bb-ffd6aa2cf9d2",
-        "Configuration": "https://smallpdf.com/file#s=b07cc63b-0327-4b5a-b2f6-59fc2ec66e2b"
+        "Configuration": "https://smallpdf.com/file#s=b07cc63b-0327-4b5a-b2f6-59fc2ec66e2b",
+        "Data Sheet": "https://smallpdf.com/file#s=57bb6fa2-cba3-4971-bbce-0049462e9165" 
     },
     {
         "Device": "Cios Spin",
         "Brochure": "https://smallpdf.com/file#s=3b4c2ced-54cb-48d4-8124-9e9f8beb5f15",
-        "Configuration": "https://smallpdf.com/file#s=9a377c59-e004-4804-8d3c-6c8f2e53309d"
+        "Configuration": "https://smallpdf.com/file#s=9a377c59-e004-4804-8d3c-6c8f2e53309d",
+        "Data Sheet": "https://smallpdf.com/file#s=57bb6fa2-cba3-4971-bbce-0049462e9165" 
     }
 ]
 
